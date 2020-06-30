fc62d7a26d3183edd08a278dddf1dcbc1f060c56
mgreen <motty.green@ecitele.com>
2020-06-30 15:27:31 +0300

    Update naming convention

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import activityModal from '../components/activityModal.vue';
+import activityModal from '../components/ActivityModal.vue';
 
 Vue.use(VueRouter);
 
